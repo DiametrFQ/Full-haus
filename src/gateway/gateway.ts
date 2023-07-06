cbf634a2c8faa6e16ba50f0a1e7f465a4d3e9c18
DiametrFQ <hohlov.03@inbox.ru>
2023-07-06 12:09:24 +0300

    Check connect and disconnect in sockets

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,22 +1,26 @@
 import { OnModuleInit } from "@nestjs/common";
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody } from "@nestjs/websockets";
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, OnGatewayDisconnect } from "@nestjs/websockets";
 import { Server } from "socket.io";
 
 
 @WebSocketGateway(1234, {})
-export class Gateway implements OnModuleInit{
+export class Gateway implements OnModuleInit, OnGatewayDisconnect{
 
     @WebSocketServer()
     server: Server
 
     onModuleInit() {
         this.server.on('connect', (socket) => {
-            console.log(socket.id)
+            console.log("Hello", socket.id, "!")
         })
     }
 
     @SubscribeMessage('events')
     handleEvent(@MessageBody() data: unknown) {
         console.log( data );
+    }
+
+    handleDisconnect(socket){
+        console.log("Bye", socket.id, "!")
     }
 }
