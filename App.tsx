3968be7de70cc710fc8fb4ee61c21f88fadcbab5
DiametrFQ <hohlov.03@inbox.ru>
2023-07-06 17:16:18 +0300

    mobile

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { io } from 'socket.io-client'
+
+const socket = io("https://test-whmf.onrender.com")
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
