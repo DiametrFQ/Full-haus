48bb9a804e8f84458cb6be4867d96c09dbdf5bcd
DiametrFQ <hohlov.03@inbox.ru>
2023-09-15 15:05:35 +0300

    no database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,21 +8,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports:[
     ConfigModule.forRoot({ isGlobal:true }),
-    TypeOrmModule.forRootAsync({
-      imports:[ConfigModule],
-      useFactory: (configServise: ConfigService) => ({
-        type: "postgres",
-        host: configServise.get('DB_HOST'),
-        port: configServise.get('DB_PORT'),
-        username: configServise.get('DB_USERNAME'),
-        password: configServise.get('DB_PASSWORD'),
-        name: configServise.get('DB_NAME'),
-        autoLoadModels: true,
-        synchronize: true,
-        entities: [__dirname + '/**/*.entity{.js, .ts}'],
-      }),
-      inject: [ConfigService]
-    }), 
     GatewayModule
   ],
   controllers: [AppController],
